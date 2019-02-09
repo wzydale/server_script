b2079a23b8198af646dabea73e43c150207763e9
Zhiyuan Wang <wzy@wzydale.cn>
2019-02-09 02:17:26 -0500

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -4,7 +4,7 @@ echo "*** 欢迎使用Wget.daletech.cn文件传输工具  ***"
 
 
 
-if [ $argv[1] ]
+if [ $argc -eq 2 ]
   then
        # write to output to tmpfile because of progress bar
        curl --progress-bar --upload-file "$1" https://transfer.sh/$(basename $1) | tee /dev/null
