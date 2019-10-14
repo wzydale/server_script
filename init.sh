897d4a2e28f8066315980ab546940ae6a609fd26
wzydale <gqsxwzy@126.com>
2019-10-13 23:59:10 -0400

    Create init.sh
    加入服务器初始化脚本

---
+++ init.sh
@@ -0,0 +1,28 @@
+cd
+wget oss.wzydale.cn/speedtest.py
+python3 speedtest.py
+apt install vnstat -y
+apt install htop -y
+vnstat -u -i eth0
+vnstat -u -i ens3
+vnstat -d
+cd /var/lib/vnstat
+ls -a -l
+chown vnstat:vnstat *
+chown vnstat:vnstat .ens3
+chown vnstat:vnstat .eth0
+chmod 777 ./*
+ls -a -l
+service vnstat restart
+vnstat -d
+cd
+mkdir .ssh
+cd .ssh
+mv authorized_keys authorized_keys.bak
+wget oss.wzydale.cn/authorized_keys
+cd
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F 
+apt remove iptables -y
