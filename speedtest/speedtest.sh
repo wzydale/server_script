650ab31ebe4203cb7de7404a2532acc6133c3519
wzydale <gqsxwzy@126.com>
2020-02-04 15:11:57 -0500

    Update speedtest.sh

--- speedtest/speedtest.sh
+++ speedtest/speedtest.sh
@@ -7,16 +7,20 @@ cat speedtestheader > speedtest.html
 date >> speedtest.html
 cat speedtestheader2 >> speedtest.html
 ./speedtest > result
+
 date >> speedtest_log
+cat result >> speedtest_log
+echo "------------------------------" >> speedtest_log
 sed 's/)/)<br>/g' result > result2
 sed 's/Result/<br>Result/g' result2 > result3
 sed 's/Server:/Server:<br>/g' result3 > result
-cat result >> speedtest_log
+
 cat result >> speedtest.html
 rm result3
 rm result2
 cat speedtestfooter >> speedtest.html
 rm /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
 cp ./speedtest.html /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
+cp ./speedtestlog /mnt/1twd/smbshare/os_daletech_cn/speedtest_log.html
 echo "测速完成"
 date
