17589cbd7bf0da1b0ea5d176974b36c28c39062f
wzydale <gqsxwzy@126.com>
2020-02-04 14:58:36 -0500

    Update speedtest.sh

--- speedtest/speedtest.sh
+++ speedtest/speedtest.sh
@@ -5,11 +5,13 @@ date >> speedtest.html
 cat speedtestheader2 >> speedtest.html
 cat resultbak > result
 date >> speedtest_log
-
 sed 's/)/)<br>/g' result > result2
-cat result2 >> speedtest_log
-cat result2 >> speedtest.html
-
+sed 's/Result/Result<br>/g' resul2 > result3
+sed 's/ by Ookla t/ /g' resul3 > result
+cat result >> speedtest_log
+cat result >> speedtest.html
+rm result3
+rm result2
 cat speedtestfooter >> speedtest.html
 rm /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
 cp ./speedtest.html /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
