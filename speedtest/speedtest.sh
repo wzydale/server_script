21e56a6be260bd8fb56f62ccc6e9310d698714ce
wzydale <gqsxwzy@126.com>
2020-02-04 14:34:41 -0500

    Update speedtest.sh

--- speedtest/speedtest.sh
+++ speedtest/speedtest.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 rm speedtest.html
 cat speedtestheader > speedtest.html
-echo date >>speedtest.html
+date >> speedtest.html
 cat speedtestheader2 >> speedtest.html
-echo "Test Result" >> speedtest.html
+./speedtest > result
+date >> speedtest_log
+cat result >> speedtest_log
+cat result >> speedtest.html
+
 cat speedtestfooter >> speedtest.html
 rm /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
 cp ./speedtest.html /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
