ebecdc26ddc0aca1f13af5979ff1ad4531f1493c
wzydale <gqsxwzy@126.com>
2020-02-04 14:55:10 -0500

    Update speedtest.sh

--- speedtest/speedtest.sh
+++ speedtest/speedtest.sh
@@ -7,8 +7,8 @@ cat resultbak > result
 date >> speedtest_log
 
 sed 's/)/)<br>/g' result > result2
-cat result >> speedtest_log
-cat result >> speedtest.html
+cat result2 >> speedtest_log
+cat result2 >> speedtest.html
 
 cat speedtestfooter >> speedtest.html
 rm /mnt/1twd/smbshare/os_daletech_cn/speedtest.html
